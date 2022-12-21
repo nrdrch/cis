fca94acbd115e2d378f45ce4d2b892d7e0d702f1
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-12-21 01:43:53 +0100

    Add files via upload

--- init.bat
+++ init.bat
@@ -1,21 +1,21 @@
 @ECHO OFF
-SET currentpath=%~dp0
 title CIS
 CLS
-Powershell -executionpolicy remotesigned -File %~dp0\CIS.ps1
+:: Powershell -executionpolicy remotesigned -File %temp%\cis\CIS.ps1
 :MENU
 CLS
 SET INPUT=
-ECHO 
+ECHO To Activate Windows : 1
+ECHO To Quit             : Q
 SET /P INPUT= Choose An Option :  
 IF /I '%INPUT%'=='1' GOTO actmeth
 IF /I '%INPUT%'=='q' GOTO Quit
 :actmeth
-IF EXIST %temp%\Microsoft-Activation-Scripts (
-    RMDIR %temp%\Microsoft-Activation-Scripts
+IF EXIST  C:\Users\%username%\Documents\Microsoft-Activation-Scripts (
+    RMDIR  C:\Users\%username%\Documents\Microsoft-Activation-Scripts
 )
-git clone https://github.com/massgravel/Microsoft-Activation-Scripts.git %temp%\Microsoft-Activation-Scripts
-cd Microsoft-Activation-Scripts\MAS\Separate-Files-VersionOnline_KMS_Activation
+git clone https://github.com/massgravel/Microsoft-Activation-Scripts.git  C:\Users\%username%\Documents\Microsoft-Activation-Scripts
+cd C:\Users\%username%\Documents\Microsoft-Activation-Scripts\MAS\Separate-Files-Version\Online_KMS_Activation
 cmd.exe /c Activate.cmd
 :Quit
 cls
